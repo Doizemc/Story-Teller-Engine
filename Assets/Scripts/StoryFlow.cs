a969285b1a62dd7d80d7e013104b6cc8cc81eb02
Mih <doizemc@yandex.ru>
2019-12-28 07:06:34 +0300

    Alpha 0.1

---
+++ Assets/Scripts/StoryFlow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Story flow")]
+public class StoryFlow : ScriptableObject
+
+{
+
+    [TextArea(10, 14)] [SerializeField] string storyText; // Текст рассказа
+    [SerializeField] AudioClip storySpeach; //Аудио рассказа
+    [SerializeField] Sprite storySprite; // Картинка  рассказа
+    [SerializeField] StoryFlow[] storyFlows; // Переходы между рассказами 
+                                             /*
+                                              Для будущего обновления сделать картинку рассказа
+                                              что бы при смене потока меняласьи картинка
+                                              */
+    public string GetStoryText()
+    {
+        return storyText;
+    }
+
+    public AudioClip GetStorySpeach()
+    {
+        return storySpeach;
+    }
+
+    public StoryFlow[] GetNextStoryFlow()
+    {
+        return storyFlows;
+    }
+    
+
+}
+
+    
---
+++ Assets/Scripts/StoryTeller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryTeller : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] Image storyImage;
+    [SerializeField] StoryFlow storyFlows;
+    StoryFlow flow;
+    AudioClip speach;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flow = storyFlows;
+        storyText.text = flow.GetStoryText();
+        speach = storyFlows.GetStorySpeach();
+        GetComponent<AudioSource>().PlayOneShot(speach);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LeftFlow()
+    {
+        var nextFlows = flow.GetNextStoryFlow();
+        flow = nextFlows[0];
+        speach = nextFlows[0].GetStorySpeach();
+        storyText.text = flow.GetStoryText();
+        GetComponent<AudioSource>().PlayOneShot(speach);
+    }
+}
