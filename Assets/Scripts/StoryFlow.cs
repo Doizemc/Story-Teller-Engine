bc6ca789649968acd5589771bbc18c5986c9f73f
Mih <doizemc@yandex.ru>
2020-06-18 18:37:52 +0300

    Second Major Update

--- Assets/Scripts/StoryFlow.cs
+++ Assets/Scripts/StoryFlow.cs
@@ -9,6 +9,8 @@ public class StoryFlow : ScriptableObject
 {
 
     [TextArea(10, 14)] [SerializeField] string storyText; // Текст рассказа
+    [SerializeField] string mihAngerLevel;  // злобнасть миха
+    [SerializeField] string dronikAngerLevel;  // злобнасть дроника
     [SerializeField] AudioClip storySpeach; //Аудио рассказа
     [SerializeField] Sprite storySprite; // Картинка  рассказа
     [SerializeField] StoryFlow[] storyFlows; // Переходы между рассказами 
@@ -19,6 +21,21 @@ public class StoryFlow : ScriptableObject
     public string GetStoryText()
     {
         return storyText;
+    }
+
+    public string GetMihAnger()
+    {
+        return mihAngerLevel;
+    }
+
+    public string GetDronikAnger()
+    {
+        return dronikAngerLevel;
+    }
+
+    public Sprite GetStorySprite()
+    {
+        return storySprite;
     }
 
     public AudioClip GetStorySpeach()
--- Assets/Scripts/StoryTeller.cs
+++ Assets/Scripts/StoryTeller.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class StoryTeller : MonoBehaviour
 {
     [SerializeField] Text storyText;
+    [SerializeField] Text mihAnger;
+    [SerializeField] Text dronikAnger;
     [SerializeField] Image storyImage;
     [SerializeField] StoryFlow storyFlows;
     StoryFlow flow;
     AudioClip speach;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +22,7 @@ public class StoryTeller : MonoBehaviour
         storyText.text = flow.GetStoryText();
         speach = storyFlows.GetStorySpeach();
         GetComponent<AudioSource>().PlayOneShot(speach);
+        Debug.Log(storyImage.sprite); 
         
         
     }
@@ -33,6 +39,21 @@ public class StoryTeller : MonoBehaviour
         flow = nextFlows[0];
         speach = nextFlows[0].GetStorySpeach();
         storyText.text = flow.GetStoryText();
+        mihAnger.text = flow.GetMihAnger();
+        dronikAnger.text = flow.GetDronikAnger();
+        storyImage.sprite = flow.GetStorySprite();
+        GetComponent<AudioSource>().PlayOneShot(speach);
+    }
+
+    public void RightFlow()
+    {
+        var nextFlows = flow.GetNextStoryFlow();
+        flow = nextFlows[1];
+        speach = nextFlows[1].GetStorySpeach();
+        storyText.text = flow.GetStoryText();
+        mihAnger.text = flow.GetMihAnger();
+        dronikAnger.text = flow.GetDronikAnger();
+        storyImage.sprite = flow.GetStorySprite();
         GetComponent<AudioSource>().PlayOneShot(speach);
     }
 }
